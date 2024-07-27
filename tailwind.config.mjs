82022b1a54215684b09b4ded88ea793be733083b
root <root@DESKTOP-RNF4OF9>
2024-07-27 21:15:00 +0000

    📦 FEAT: add vars

---
+++ tailwind.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		container: {
+			center: true,
+		  },
+		extend: {
+			
+			colors:{
+				primary: 'rgb(var(--blue) / <alpha-value>)',
+				
+				gray:{
+					100: 'rgb(var(--grey-1) / <alpha-value>)', 	
+					200: 'rgb(var(--shadow-players) / <alpha-value>)', 	
+					300: 'rgb(var(--button-grey-2) / <alpha-value>)', 	
+					500: 'rgb(var(--lines-bg) / <alpha-value>)', 	
+					600: 'rgb(var(--button-grey) / <alpha-value>)', 	
+					700: 'rgb(var(--play) / <alpha-value>)', 	
+					400: 'rgb(var(--dark-2) / <alpha-value>)', 	
+					800: 'rgb(var(--bg-dark) / <alpha-value>)', 	
+				},
+				// 900: 'rgb(var(--black) / <alpha-value>)', 	
+				white:{
+					"DEFAULT": 'rgb(var(--white) / <alpha-value>)', 	
+					200: 'rgb(var(--bg-white-2) / <alpha-value>)', 	
+					300: 'rgb(var(--white-2) / <alpha-value>)', 	
+					400: 'rgb(var(--luz-focal) / <alpha-value>)', 	
+				}
+			}
+		},
+	},
+	plugins: [],
+}
